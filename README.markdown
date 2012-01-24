e1f474c0bbba40314a816110f6554865206cfb24
Stefan Wallin <stefan.w@festiz.com>
2012-01-24 22:16:00 +0100

    A lot of cleaning up for v. 0.2. Still a lot of cleaning up to do.
    * Actually support transforming a subset of a document.
    * Allow passing in a document node to use as context.
    * Fix overflow-issue where one could hover outside and still trigger hover styles.
    * Restructured code a bit
    * Added some testcases to pretty page
    * Added timing to demo page(3 elements converted in roughly 5ms.)
    * Removed unneccessary ID for now
    * Don't both use detach and remove for removing old elements.
    * Use el.find("") instead of $("",el).

--- README.markdown
+++ README.markdown
@@ -3,12 +3,37 @@
 ## [Online demo](http://lab.festiz.com/uploader/demo.html)
 
 ## The Gist:
-The HTML5 File Upload improver is a script that runs and transforms any input[type=file]-field in a subset of your code and improves on them progressivly.
+The HTML5 File Upload improver is a script that runs and transforms any input[type=file]-field in a subset of your code and improves on them progressivly. Each uploader element that is modified is done with only one document modification.
+
+## Requirements
+<ul>
+	<li>jQuery, probably 1.7, but 1.5 could work</li>
+</ul>
+
+## Tested on
+<ul>
+	<li>Mac OS X 10.7 with Google Chrome 16 running jQuery 1.7.1</li>
+</ul>
 
 ## Licensing:
 HTML5 Uploader by Stefan Wallin is licensed under a Creative Commons Attribution 3.0 Unported License.
+I take no responsibility of what you do with my code or what it does to you nor the entities that you represent.
 
 ## Changelog:
+
+### v.0.2
+<ul>
+	<li>Actually support transforming a subset of a document.</li>
+	<li>Allow passing in a document node to use as context.</li>
+	<li>Fix overflow-issue where one could hover outside and still trigger hover styles.</li>
+	<li>Restructured code a bit</li>
+	<li>Added some testcases to pretty page</li>
+	<li>Added timing to demo page(3 elements converted in roughly 5ms.)</li>
+	<li>Removed unneccessary ID for now</li>
+	<li>Don't both use detach and remove for removing old elements.</li>
+	<li>Use el.find("") instead of $("",el).</li>
+</ul>
+
 
 ### v.0.1
 <ul>
--- js/script.js
+++ js/script.js
@@ -3,48 +3,42 @@
    License: HTML5 Uploader by Stefan Wallin is licensed under a Creative Commons Attribution 3.0 Unported License.
 */
 function cancelEvent() { return false; } 
-function addFiles(files,uri,el){
+function addFiles(files,supportsUri,el){
 	var file=null;
-	if (files.length > 0) {
+	if (files.length) {
 		for(var i = files.length; i>0; i--){
 			file = files[i-1];
 			if (typeof FileReader !== "undefined" && file.type.indexOf("image") != -1) {
 				var reader = new FileReader();
 				// Note: addEventListener doesn't work in Google Chrome for this event
 				reader.onload = function (event) {
-					$("img",el).attr("src",event.target.result).show();
-					$("p",el).hide();
-					$("a",el).show();
+					el.find("img").attr("src",event.target.result).show();
+					el.find("p").hide();
+					el.find("a").show();
 				};
 				reader.readAsDataURL(file);
 			}			
 		}
 	}
 }
-function createUploaderElem(i,olEl,drag,uri){
+function createUploaderElem(i,olEl,supportsDrag,supportsUri){
 	var olName,olId,olAccept,olMultiple,el,a,p,img,input = null;
 	olName = olEl.attr("name");
 	olId = olEl.attr("id");
 	olAccept = olEl.attr("accept") || "image/jpeg,image/jpg,image/gif,image/png";
 	olMultiple = olEl.attr("multiple");
 	
-	el=$(document.createElement("div"));
-	el.id="drop"+i;
-	el.attr("class","droploc");
-	
-	a=$(document.createElement("a"));
-	a.attr("href","#");
-	a.attr("title","Remove this image");
-	a.text("×");
-	a.attr("style","display:none");
-	a.click(function(event){
+	el=$("<div>").attr("class","droploc");
+	a=$("<a>").attr("href","#").attr("title","Remove this image").attr("style","display:none").text("×").click(function(event){
+		// event.preventDefault();
+		// $(this).parent().toggleClass('addimage').toggleClass("imageadded")
 		event.cancelBubble = true;
 		var a,p,img,input,droploc;
 		droploc = $(this).parent();
-		a=$("a",droploc);
-		p=$("p",droploc);
-		img=$("img",droploc);
-		input=$("input",droploc);
+		a=droploc.find("a");
+		p=droploc.find("p");
+		img=droploc.find("img");
+		input=droploc.find("input");
 		
 		a.hide();
 		img.hide();
@@ -52,23 +46,21 @@ function createUploaderElem(i,olEl,drag,uri){
 		return false;
 	});
 	
-	p=$(document.createElement("p"));
-	p.text("Add image");
+	p=$("<p>").text("Add image");
+	img=$("<img>").attr("style","display:none");
 	
-	img=$(document.createElement("img"));
-	img.attr("style","display:none");
-	
-	input=$(document.createElement("input"));
-	input.attr("id",olId || olName);
+	input=$("<input>");
+	input.attr("id",olId || olName)
 	input.attr("name",olName);
 	input.attr("type","file");
-	if(olMultiple != undefined)
-		input.attr("multiple","multiple");
 	input.attr(olAccept);
 	input.change(function(event){
 		el=$(event.srcElement).parent();
-		addFiles(event.target.files,uri,el);
+		addFiles(event.target.files,supportsUri,el);
 	})
+
+	if(olMultiple != undefined)
+		input.attr("multiple","multiple");
 	
 	el.append(a);
 	el.append(p);
@@ -76,17 +68,17 @@ function createUploaderElem(i,olEl,drag,uri){
 	el.append(input);
 	
 	
-	if(drag){
+	if(supportsDrag){
 		/* We need to cancel the events to accept whatever is being dropped. */
-		el.get(0).ondragenter = cancelEvent; //This one is for IE legacy(inventors of API). 
-		el.get(0).ondragover = cancelEvent; //This one is for the standards API.
+		el.get(0).onsupportsDragenter = cancelEvent; //This one is for IE legacy(inventors of API). 
+		el.get(0).onsupportsDragover = cancelEvent; //This one is for the standards API.
 		el.get(0).ondrop = function (event) {
 			event = event || window.event; //IE doesn't pass the event handler, so we need to retrieve it
-			var msg = $("p",el);
-			var img = $("img",el);
-			var link = $("a",el);
+			var msg = el.find("p");
+			var img = el.find("img");
+			var link = el.find("a");
 			files =  event.dataTransfer.files;	
-			addFiles(files,uri,el);
+			addFiles(files,supportsUri,el);
 			event.cancelBubble = true; //Cancel bubbling opening new windows in IE
 			return false; // To prevent FF to follow any href.
 		};
@@ -97,30 +89,30 @@ function setupFileUploaders(container){
 	// JS is supported, lets get advanced
 	var numFiles = 0;
 	var olEl=null;
-	var upContainer = $("#"+container);
+	container = $(container);
 	
-	// Detect data URI support
-	var uri = false;
+	// Detect data supportsUri support
+	var supportsUri = false;
 	var data = new Image();
 	data.onload = data.onerror = function(){
 		if(this.width != 1 || this.height != 1){
-			uri = true;
+			supportsUri = true;
 		}
 	}
 	data.src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";
 	
-	// Detect drag n drop
-	var drag = false;
+	// Detect supportsDrag n drop
+	var supportsDrag = false;
 	if('draggable' in document.createElement('span')) {
-		drag = true
+		supportsDrag = true
 	}
 	
 	
-	$("input[type=file]").each(function(){
+	container.find("input[type=file]").each(function(){
 		numFiles++;
 		olEl = $(this);		
-		olEl.before(createUploaderElem(numFiles,olEl,drag,uri));
-		olEl.detach().remove();
+		olEl.before(createUploaderElem(numFiles,olEl,supportsDrag,supportsUri));
+		olEl.remove();
 
 	});
 	
